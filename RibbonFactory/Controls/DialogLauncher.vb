7519d34285c82db93bc5de47178ce36cd6fc1a23
Scott K <georgelarry99@gmail.com>
2021-12-11 14:20:28 +0100

    Add support for DialogLaunchers (Issue #6).

---
+++ RibbonFactory/Controls/DialogLauncher.vb
@@ -0,0 +1,25 @@
+﻿Namespace Controls
+    Public NotInheritable Class DialogLauncher
+        Inherits RibbonElement
+
+        Private ReadOnly _button As Button
+
+        Friend Sub New(button As Button)
+            _button = button
+        End Sub
+
+        Public Overrides ReadOnly Property ID As String
+            Get
+                Return String.Empty
+            End Get
+        End Property
+
+        Public Overrides ReadOnly Property XML As String
+            Get
+                Return String.Join(Environment.NewLine, "<dialogBoxLauncher>", _button.XML, "</dialogBoxLauncher>")
+            End Get
+        End Property
+
+    End Class
+
+End NameSpace
