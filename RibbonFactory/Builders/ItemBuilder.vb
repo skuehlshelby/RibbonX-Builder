b30d693a6bb11bbc3f98a819f714bc8099d0e405
Scott K <georgelarry99@gmail.com>
2021-12-15 18:15:49 +0100

    Made all properties of the Item object always read/write.

--- RibbonFactory/Builders/ItemBuilder.vb
+++ RibbonFactory/Builders/ItemBuilder.vb
@@ -1,64 +1,65 @@
 ﻿Imports System.Drawing
-Imports RibbonFactory.BuilderInterfaces
 Imports RibbonFactory.Controls
-Imports RibbonFactory.Enums.ImageMSO
 Imports RibbonFactory.RibbonAttributes
+Imports RibbonFactory.Utilities
 Imports stdole
 
 Namespace Builders
     Public NotInheritable Class ItemBuilder
-        Implements ILabelScreenTipSuperTip(Of ItemBuilder)
-
-        Private ReadOnly _builder As ControlBuilder
+        Private ReadOnly _id As String
+        Private _label As String
+        Private _screenTip As String
+        Private _superTip As String
+        Private _image As IPictureDisp
+        Private _synchronizeLabelAndScreenTip As Boolean
 
         Public Sub New()
-            Dim defaultProvider As IDefaultProvider = New DefaultProvider(Of Item)
-            Dim attributeGroupBuilder As AttributeGroupBuilder = New AttributeGroupBuilder()
-            attributeGroupBuilder.SetDefaults(defaultProvider)
-            _builder = new ControlBuilder(attributeGroupBuilder)
+            _id = IdManager.GetID(Of Item)
+            _label = String.Empty
+            _screenTip = String.Empty
+            _superTip = String.Empty
+            _image = Nothing
+            _synchronizeLabelAndScreenTip = False
         End Sub
 
         Public Function Build(Optional tag As Object = Nothing) As Item
-            Return New Item(_builder.Build(), tag)
+            Return New Item(_id, _label, _screenTip, _superTip, _image, _synchronizeLabelAndScreenTip, tag)
         End Function
 
-        Public Function WithLabel(label As String, Optional copyToScreenTip As Boolean = True) As ItemBuilder Implements ILabelScreenTipSuperTip(Of ItemBuilder).WithLabel
-            _builder.WithLabel(label, copyToScreenTip)
+        Public Function WithLabel(label As String, Optional synchronizeLabelAndScreenTip As Boolean = True) As ItemBuilder
+            _label = label
+
+            _synchronizeLabelAndScreenTip = synchronizeLabelAndScreenTip
+
+            If synchronizeLabelAndScreenTip Then
+                _screenTip = _label
+            End If
+
             Return Me
         End Function
 
-        Public Function WithLabel(label As String, callback As FromControl(Of String), Optional copyToScreenTip As Boolean = True) As ItemBuilder Implements ILabelScreenTipSuperTip(Of ItemBuilder).WithLabel
-            _builder.WithLabel(label, callback, copyToScreenTip)
+        Public Function WithScreenTip(screenTip As String) As ItemBuilder
+            _screenTip = screenTip
+
+            If _synchronizeLabelAndScreenTip Then
+                _label = _screenTip
+            End If
+
             Return Me
         End Function
 
-        Public Function WithScreenTip(screenTip As String) As ItemBuilder Implements ILabelScreenTipSuperTip(Of ItemBuilder).WithScreenTip
-            _builder.WithScreentip(screenTip)
-            Return Me
-        End Function
-
-        Public Function WithScreenTip(screenTip As String, callback As FromControl(Of String)) As ItemBuilder Implements ILabelScreenTipSuperTip(Of ItemBuilder).WithScreenTip
-            _builder.WithScreentip(screenTip, callback)
-            Return Me
-        End Function
-
-        Public Function WithSuperTip(superTip As String) As ItemBuilder Implements ILabelScreenTipSuperTip(Of ItemBuilder).WithSuperTip
-            _builder.WithSupertip(superTip)
-            Return Me
-        End Function
-
-        Public Function WithSuperTip(superTip As String, callback As FromControl(Of String)) As ItemBuilder Implements ILabelScreenTipSuperTip(Of ItemBuilder).WithSuperTip
-            _builder.WithSupertip(superTip, callback)
+        Public Function WithSuperTip(superTip As String) As ItemBuilder
+            _superTip = superTip
             Return Me
         End Function
 
         Public Function WithImage(image As Bitmap) As ItemBuilder
-            _builder.WithItemImage(image)
+            _image = New AdapterForIPicture(image)
             Return Me
         End Function
 
         Public Function WithImage(image As Icon) As ItemBuilder
-            _builder.WithItemImage(image)
+            _image = New AdapterForIPicture(image)
             Return Me
         End Function
     End Class
--- RibbonFactory/Controls/Item.vb
+++ RibbonFactory/Controls/Item.vb
@@ -1,6 +1,5 @@
 ﻿
 Imports RibbonFactory.ControlInterfaces
-Imports RibbonFactory.RibbonAttributes
 Imports stdole
 
 Namespace Controls
@@ -15,59 +14,80 @@ Namespace Controls
         Implements ISuperTip
         Implements IImage
 
-        Private ReadOnly _attributes As AttributeSet
+        Private _label As String
+        Private _screenTip As String
+        Private _superTip As String
+        Private _image As IPictureDisp
+        Private ReadOnly _synchronizeLabelAndScreenTip As Boolean
 
-        Friend Sub New(attributes As AttributeSet, Optional tag As Object = Nothing)
+        Friend Sub New(id As String, label As String, screenTip As String, superTip As String, image As IPictureDisp, Optional synchronizeLabelAndScreenTip As Boolean = True, Optional tag As Object = Nothing)
             MyBase.New(tag)
-            _attributes = attributes
-            AddHandler _attributes.AttributeChanged, AddressOf RefreshNeeded
+            Me.ID = id
+            _label = label
+            _screenTip = screenTip
+            _superTip = superTip
+            _image = image
+            _synchronizeLabelAndScreenTip = synchronizeLabelAndScreenTip
         End Sub
 
         Public Overrides ReadOnly Property ID As String
-            Get
-                Return _attributes.ReadOnlyLookup(Of String)(AttributeName.Id).GetValue()
-            End Get
-        End Property
 
         Public Overrides ReadOnly Property XML As String
             Get
-                Return $"<item { _attributes }/>"
+                Return String.Empty
             End Get
         End Property
 
         Public Property Label As String Implements ILabel.Label
             Get
-                Return _attributes.ReadOnlyLookup(Of String)(AttributeName.Label).GetValue()
+                Return _label
             End Get
             Set
-                _attributes.ReadWriteLookup(Of String)(AttributeName.GetLabel).SetValue(Value)
+                If Not _label.Equals(value, StringComparison.OrdinalIgnoreCase) Then
+                    _label = value
+
+                    If _synchronizeLabelAndScreenTip Then
+                        _screenTip = value
+                    End If
+
+                    RefreshNeeded()
+                End If
             End Set
         End Property
 
         Public Property ScreenTip As String Implements IScreenTip.ScreenTip
             Get
-                Return _attributes.ReadOnlyLookup(Of String)(AttributeName.Screentip).GetValue()
+                Return _screenTip
             End Get
             Set
-                _attributes.ReadWriteLookup(Of String)(AttributeName.GetScreentip).SetValue(Value)
+                If Not _screenTip.Equals(value, StringComparison.OrdinalIgnoreCase) Then
+                    _screenTip = value
+                    RefreshNeeded()
+                End If
             End Set
         End Property
 
         Public Property SuperTip As String Implements ISupertip.Supertip
             Get
-                Return _attributes.ReadOnlyLookup(Of String)(AttributeName.Supertip).GetValue()
+                Return _superTip
             End Get
             Set
-                _attributes.ReadWriteLookup(Of String)(AttributeName.GetSupertip).SetValue(Value)
+                If Not _superTip.Equals(value, StringComparison.OrdinalIgnoreCase) Then
+                    _superTip = value
+                    RefreshNeeded()
+                End If
             End Set
         End Property
 
         Public Property Image As IPictureDisp Implements IImage.Image
             Get
-                Return _attributes.ReadOnlyLookup(Of IPictureDisp)(AttributeName.GetImage).GetValue()
+                Return _image
             End Get
             Set
-                _attributes.ReadWriteLookup(Of IPictureDisp)(AttributeName.GetImage).SetValue(Value)
+                If Not ReferenceEquals(_image, value) Then
+                    _image = value
+                    RefreshNeeded()
+                End If
             End Set
         End Property
 
