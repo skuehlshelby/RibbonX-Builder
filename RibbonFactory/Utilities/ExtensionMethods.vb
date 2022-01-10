3f0a8ef7cf5fcc8e394dce647f5e4c2aff543647
Scott K <georgelarry99@gmail.com>
2022-01-10 08:41:59 +0100

    Consolidate extension methods.

--- RibbonFactory/Utilities/ExtensionMethods.vb
+++ RibbonFactory/Utilities/ExtensionMethods.vb
@@ -1,8 +1,33 @@
-﻿Imports System.Runtime.CompilerServices
+﻿Imports System.IO
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Xml
+Imports System.Xml.Schema
+Imports RibbonFactory.Containers
+Imports RibbonFactory.Utilities.Testing
 
 Namespace Utilities
 
     Friend Module ExtensionMethods
+
+        <Extension>
+        Public Function GetErrors(ribbon As Ribbon) As RibbonErrorLog
+            Return GetErrors(ribbon.RibbonX)
+        End Function
+
+        Public Function GetErrors(ribbonX As String) As RibbonErrorLog
+            Return New RibbonErrorLog(ribbonX, GetCustomUIVersion2009())
+        End Function
+
+        Public Function GetCustomUIVersion2009() As XmlSchema
+            Using reader As StreamReader = New StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("RibbonFactory.RibbonX.xsd"))
+                Return XmlSchema.Read(XmlReader.Create(reader), AddressOf ValidationEventHandler)
+            End Using
+        End Function
+
+        Private Sub ValidationEventHandler(sender As Object, e As ValidationEventArgs)
+            Console.WriteLine($"{e.Severity.ToString().ToUpper}: {e.Message}")
+        End Sub
     
         <Extension()>
         Public Function CamelCase(input As Object) As String 
@@ -38,5 +63,28 @@ Namespace Utilities
 
             Return False
         End Function
+
+        <Extension()>
+        Public Function ImplementsGenericInterface(type As Type, generic As Type) As Boolean
+            While type IsNot Nothing AndAlso type IsNot GetType(Object)
+                For Each iFace As Type In type.GetInterfaces()
+                    Dim check As Type = If(iFace.IsGenericType, iFace.GetGenericTypeDefinition(), iFace)
+
+                    If check Is generic Then
+                        Return True
+                    End If
+                Next
+
+                type = type.BaseType
+            End While
+
+            Return False
+        End Function
+
+        <Extension()>
+        Public Function NextBoolean(random As Random) As Boolean
+            Return random.Next Mod 2 = 0
+        End Function
+
     End Module
 End NameSpace
