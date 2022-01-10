f8629ca70eeff4d1f094b6aaf1895edd19fe3564
Scott K <georgelarry99@gmail.com>
2022-01-10 08:36:54 +0100

    Move general testing facilities to the main project.

---
+++ RibbonFactory/Utilities/Testing/OfficeHostAppilcation.vb
@@ -0,0 +1,196 @@
+﻿Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports Microsoft.Office.Core
+Imports RibbonFactory.ControlInterfaces
+
+Namespace Utilities.Testing
+
+	''' <summary>
+	''' A test class for simulating requests from an MS Office host application.
+	''' </summary>
+	Public Class OfficeHostAppilcation
+		Implements IRibbonUI
+
+		Private ReadOnly _controls As IDictionary(Of String, RibbonControl)
+
+		Public Sub New(ribbonExtensibility As IRibbonExtensibility)
+			Dim temp As String = ribbonExtensibility.GetCustomUI(String.Empty)
+			Dim ribbonX As XDocument = XDocument.Parse(temp)
+
+			_controls = ribbonX.
+				Root.
+				Descendants().
+				Where(Function(descendant) descendant.Attribute("id") IsNot Nothing).
+				Select(Function(element) New RibbonControl(element, ribbonExtensibility)).
+				ToDictionary(Function(control) control.Id)
+		End Sub
+
+		''' <summary>
+		''' Simulates a click on a ribbon element, as performed by a user in Excel.
+		''' </summary>
+		''' <param name="element"></param>
+		Public Sub Click(element As RibbonElement)
+			_controls(element.ID).Click()
+		End Sub
+
+		Public Function IsDataSynchronized(element As RibbonElement) As Boolean
+			Return _controls(element.ID).CallbackReturnValuesMatchRibbonElementPropertyValues(element)
+		End Function
+
+#Region "IRibbonUI Members"
+
+		Public Sub Invalidate() Implements IRibbonUI.Invalidate
+			For Each control As RibbonControl In _controls.Values
+				control.QueryControlProperties()
+			Next
+		End Sub
+
+		Public Sub InvalidateControl(controlID As String) Implements IRibbonUI.InvalidateControl
+			_controls(controlID).QueryControlProperties()
+		End Sub
+
+		Public Sub InvalidateControlMso(ControlID As String) Implements IRibbonUI.InvalidateControlMso
+			'Do Nothing
+		End Sub
+
+		Public Sub ActivateTab(ControlID As String) Implements IRibbonUI.ActivateTab
+			'Do Nothing
+		End Sub
+
+		Public Sub ActivateTabMso(ControlID As String) Implements IRibbonUI.ActivateTabMso
+			'Do Nothing
+		End Sub
+
+		Public Sub ActivateTabQ(ControlID As String, [Namespace] As String) Implements IRibbonUI.ActivateTabQ
+			'Do Nothing
+		End Sub
+
+#End Region
+
+		Private Class RibbonControl
+			Implements IRibbonControl
+
+			Private ReadOnly element As XElement
+			Private ReadOnly extensibility As IRibbonExtensibility
+
+			Public Sub New(element As XElement, ribbonExtensibility As IRibbonExtensibility)
+				Me.element = element
+				extensibility = ribbonExtensibility
+			End Sub
+
+			Public ReadOnly Property Id As String Implements IRibbonControl.Id
+				Get
+					Return If(element.Attribute("id")?.Value, String.Empty)
+				End Get
+			End Property
+
+			Private ReadOnly Property Context As Object Implements IRibbonControl.Context
+				Get
+					Return Nothing
+				End Get
+			End Property
+
+			Private ReadOnly Property Tag As String Implements IRibbonControl.Tag
+				Get
+					Return element.Attribute("tag")?.Value
+				End Get
+			End Property
+
+			Public Sub QueryControlProperties()
+				For Each attribute As XAttribute In element.Attributes()
+					Dim callback As String = Nothing
+
+					If IsCallback(attribute, Nothing, callback) Then
+						InvokeMethod(callback)
+					End If
+				Next
+			End Sub
+
+			Public Sub Click()
+				Dim action As XAttribute = element.Attribute("onAction")
+				InvokeMethod(action.Value)
+			End Sub
+
+			Public Sub Type(text As String)
+				Dim action As XAttribute = element.Attribute("onChange")
+				InvokeMethod(action.Value, text)
+			End Sub
+
+			Public Function CallbackReturnValuesMatchRibbonElementPropertyValues(control As RibbonElement) As Boolean
+				For Each attribute As XAttribute In element.Attributes()
+					Dim name As String = Nothing
+					Dim callback As String = Nothing
+
+					'TODO Make sure everything is represented here
+					If IsCallback(attribute, name, callback) Then
+						Select Case name
+							Case "getLabel"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, ILabel).Label) Then
+									Return False
+								End If
+							Case "getShowLabel"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, IShowLabel).ShowLabel) Then
+									Return False
+								End If
+							Case "getScreentip"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, IScreenTip).ScreenTip) Then
+									Return False
+								End If
+							Case "getSupertip"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, ISuperTip).SuperTip) Then
+									Return False
+								End If
+							Case "getDescription"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, IDescription).Description) Then
+									Return False
+								End If
+							Case "getKeytip"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, IKeyTip).KeyTip) Then
+									Return False
+								End If
+							Case "getEnabled"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, IEnabled).Enabled) Then
+									Return False
+								End If
+							Case "getVisible"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, IVisible).Visible) Then
+									Return False
+								End If
+							Case "getImage"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, IImage).Image) Then
+									Return False
+								End If
+							Case "getSize"
+								If Not InvokeMethod(callback).Equals(DirectCast(control, ISize).Size) Then
+									Return False
+								End If
+						End Select
+					End If
+				Next
+
+				Return True
+			End Function
+
+			Private Function IsCallback(attribute As XAttribute, <Out> ByRef attributeName As String, <Out> ByRef callback As String) As Boolean
+				If attribute.Name.LocalName.StartsWith("get", StringComparison.OrdinalIgnoreCase) Then
+					attributeName = attribute.Name.LocalName
+					callback = attribute.Value
+					Return True
+				Else
+					attributeName = Nothing
+					callback = Nothing
+					Return False
+				End If
+			End Function
+
+			Private Function InvokeMethod(name As String, ParamArray args() As Object) As Object
+				Return extensibility.
+					GetType().
+					GetMethod(name, BindingFlags.Public Or BindingFlags.Instance).
+					Invoke(extensibility, New Object() {Me}.Concat(args).ToArray())
+			End Function
+		End Class
+
+	End Class
+
+End Namespace
---
+++ RibbonFactory/Utilities/Testing/RandomControlGenerator.vb
@@ -0,0 +1,262 @@
+﻿Imports System.Reflection
+Imports RibbonFactory.BuilderInterfaces
+Imports RibbonFactory.Builders
+Imports RibbonFactory.Containers
+Imports RibbonFactory.Controls
+Imports RibbonFactory.Enums
+Imports RibbonFactory.Utilities
+
+Public NotInheritable Class RandomControlGenerator
+
+	Private ReadOnly random As Random = New Random()
+	Private ReadOnly callbacks As ICreateCallbacks
+
+	Public Sub New(callbacks As ICreateCallbacks)
+		Me.callbacks = callbacks
+	End Sub
+
+	Public Function MakeButton() As Button
+		Return MakeControl(Function() New ButtonBuilder())
+	End Function
+
+	Public Function MakeCheckbox() As CheckBox
+		Return MakeControl(Function() New CheckBoxBuilder())
+	End Function
+
+	Public Function MakeComboBox() As ComboBox
+		Return MakeControl(Function() New ComboBoxBuilder())
+	End Function
+
+	Public Function MakeEditBox() As EditBox
+		Return MakeControl(Function() New EditBoxBuilder())
+	End Function
+
+	Public Function MakeLabelControl() As LabelControl
+		Return MakeControl(Function() New LabelControlBuilder())
+	End Function
+
+	Public Function MakeToggleButton() As ToggleButton
+		Return MakeControl(Function() New ToggleButtonBuilder())
+	End Function
+
+	Public Function MakeSplitButton() As SplitButton
+		Return MakeControl(Function() New SplitButtonBuilder())
+	End Function
+
+	Public Function MakeDropDown() As DropDown
+		Return MakeControl(Function() New DropDownBuilder())
+	End Function
+
+	Public Function MakeGallery() As Gallery
+		Return MakeControl(Function() New GalleryBuilder())
+	End Function
+
+	Private Function MakeControl(Of T As RibbonElement)(builderInitializer As Func(Of IBuilder(Of T))) As T
+		Dim builder As IBuilder(Of T) = builderInitializer.Invoke()
+
+		Dim type As Type = builder.GetType()
+
+		Dim method As MethodInfo = Nothing
+
+		If builder.GetType().ImplementsGenericInterface(GetType(ILabelOnly(Of ))) Then
+			method = type.GetMethod(NameOf(ILabelOnly(Of Object).WithLabel), New Type() {})
+
+			method.Invoke(builder, New Object() {"A Sweet Label"})
+		End If
+
+		If builder.GetType().ImplementsGenericInterface(GetType(ILabelScreenTipSuperTip(Of ))) Then
+			If random.NextBoolean() Then
+				method = type.GetMethod(NameOf(ILabelScreenTipSuperTip(Of Object).WithLabel), New Type() {GetType(String), GetType(Boolean)})
+
+				method.Invoke(builder, New Object() {"A Sweet Label", random.NextBoolean()})
+			Else
+				method = type.GetMethod(NameOf(ILabelScreenTipSuperTip(Of Object).WithLabel), New Type() {GetType(String), GetType(FromControl(Of String)), GetType(Boolean)})
+
+				Dim callback As FromControl(Of String) = New FromControl(Of String)(AddressOf callbacks.GetLabel)
+
+				method.Invoke(builder, New Object() {"A Different, But Still Sweet Label", callback, random.NextBoolean()})
+			End If
+
+			If random.NextBoolean() Then
+				If random.NextBoolean() Then
+					method = type.GetMethod(NameOf(ILabelScreenTipSuperTip(Of Object).WithScreenTip), New Type() {GetType(String)})
+
+					method.Invoke(builder, New Object() {"The Screentip"})
+				Else
+					method = type.GetMethod(NameOf(ILabelScreenTipSuperTip(Of Object).WithScreenTip), New Type() {GetType(String), GetType(FromControl(Of String))})
+
+					Dim callback As FromControl(Of String) = New FromControl(Of String)(AddressOf callbacks.GetScreenTip)
+
+					method.Invoke(builder, New Object() {"A Different Screentip", callback})
+				End If
+			End If
+
+			If random.NextBoolean() Then
+				method = type.GetMethod(NameOf(ILabelScreenTipSuperTip(Of Object).WithSuperTip), New Type() {GetType(String)})
+
+				method.Invoke(builder, New Object() {"The Supertip"})
+			Else
+				method = type.GetMethod(NameOf(ILabelScreenTipSuperTip(Of Object).WithSuperTip), New Type() {GetType(String), GetType(FromControl(Of String))})
+
+				Dim callback As FromControl(Of String) = New FromControl(Of String)(AddressOf callbacks.GetSuperTip)
+
+				method.Invoke(builder, New Object() {"A Different Supertip", callback})
+			End If
+		End If
+
+		If builder.GetType().ImplementsGenericInterface(GetType(IDescription(Of ))) Then
+			If random.NextBoolean() Then
+				If random.NextBoolean() Then
+					method = type.GetMethod(NameOf(IDescription(Of Object).WithDescription), New Type() {GetType(String)})
+
+					method.Invoke(builder, New Object() {"The Description"})
+				Else
+					method = type.GetMethod(NameOf(IDescription(Of Object).WithDescription), New Type() {GetType(String), GetType(FromControl(Of String))})
+
+					Dim callback As FromControl(Of String) = New FromControl(Of String)(AddressOf callbacks.GetDescription)
+
+					method.Invoke(builder, New Object() {"A Different Description", callback})
+				End If
+			End If
+		End If
+
+		If builder.GetType().ImplementsGenericInterface(GetType(IKeyTip(Of ))) Then
+			If random.NextBoolean() Then
+				If random.NextBoolean() Then
+					method = type.GetMethod(NameOf(IKeyTip(Of Object).WithKeyTip), New Type() {GetType(KeyTip)})
+
+					method.Invoke(builder, New Object() {KeyTip.NextAvailable()})
+				Else
+					method = type.GetMethod(NameOf(IKeyTip(Of Object).WithKeyTip), New Type() {GetType(KeyTip), GetType(FromControl(Of KeyTip))})
+
+					Dim callback As FromControl(Of KeyTip) = New FromControl(Of KeyTip)(AddressOf callbacks.GetKeyTip)
+
+					method.Invoke(builder, New Object() {KeyTip.NextAvailable(), callback})
+				End If
+			End If
+		End If
+
+		If builder.GetType().ImplementsGenericInterface(GetType(IVisible(Of ))) Then
+			If random.NextBoolean() Then
+				If random.NextBoolean() Then
+					If random.NextBoolean() Then
+						method = type.GetMethod(NameOf(IVisible(Of Object).Visible), New Type() {})
+
+						method.Invoke(builder, New Object() {})
+					Else
+						method = type.GetMethod(NameOf(IVisible(Of Object).Visible), New Type() {GetType(FromControl(Of Boolean))})
+
+						Dim callback As FromControl(Of Boolean) = New FromControl(Of Boolean)(AddressOf callbacks.GetVisible)
+
+						method.Invoke(builder, New Object() {callback})
+					End If
+				Else
+					If random.NextBoolean() Then
+						method = type.GetMethod(NameOf(IVisible(Of Object).Invisible), New Type() {})
+
+						method.Invoke(builder, New Object() {})
+					Else
+						method = type.GetMethod(NameOf(IVisible(Of Object).Invisible), New Type() {GetType(FromControl(Of Boolean))})
+
+						Dim callback As FromControl(Of Boolean) = New FromControl(Of Boolean)(AddressOf callbacks.GetVisible)
+
+						method.Invoke(builder, New Object() {callback})
+					End If
+				End If
+			End If
+		End If
+
+		If builder.GetType().IsDerivedFromGenericType(GetType(IEnabled(Of ))) Then
+			If random.NextBoolean() Then
+				If random.NextBoolean() Then
+					If random.NextBoolean() Then
+						method = type.GetMethod(NameOf(IEnabled(Of Object).Enabled), New Type() {})
+
+						method.Invoke(builder, New Object() {})
+					Else
+						method = type.GetMethod(NameOf(IEnabled(Of Object).Enabled), New Type() {GetType(FromControl(Of Boolean))})
+
+						Dim callback As FromControl(Of Boolean) = New FromControl(Of Boolean)(AddressOf callbacks.GetEnabled)
+
+						method.Invoke(builder, New Object() {callback})
+					End If
+				Else
+					If random.NextBoolean() Then
+						method = type.GetMethod(NameOf(IEnabled(Of Object).Disabled), New Type() {})
+
+						method.Invoke(builder, New Object() {})
+					Else
+						method = type.GetMethod(NameOf(IEnabled(Of Object).Disabled), New Type() {GetType(FromControl(Of Boolean))})
+
+						Dim callback As FromControl(Of Boolean) = New FromControl(Of Boolean)(AddressOf callbacks.GetEnabled)
+
+						method.Invoke(builder, New Object() {callback})
+					End If
+				End If
+			End If
+		End If
+
+		If builder.GetType().ImplementsGenericInterface(GetType(ISize(Of ))) Then
+			If random.NextBoolean() Then
+				If random.NextBoolean() Then
+					If random.NextBoolean() Then
+						method = type.GetMethod(NameOf(ISize(Of Object).Normal), New Type() {})
+
+						method.Invoke(builder, New Object() {})
+					Else
+						method = type.GetMethod(NameOf(ISize(Of Object).Normal), New Type() {GetType(FromControl(Of ControlSize))})
+
+						Dim callback As FromControl(Of ControlSize) = New FromControl(Of ControlSize)(AddressOf callbacks.GetSize)
+
+						method.Invoke(builder, New Object() {callback})
+					End If
+				Else
+					If random.NextBoolean() Then
+						method = type.GetMethod(NameOf(ISize(Of Object).Large), New Type() {})
+
+						method.Invoke(builder, New Object() {})
+					Else
+						method = type.GetMethod(NameOf(ISize(Of Object).Large), New Type() {GetType(FromControl(Of ControlSize))})
+
+						Dim callback As FromControl(Of ControlSize) = New FromControl(Of ControlSize)(AddressOf callbacks.GetSize)
+
+						method.Invoke(builder, New Object() {callback})
+					End If
+				End If
+			End If
+		End If
+
+		If builder.GetType().ImplementsGenericInterface(GetType(IMaxLength(Of ))) Then
+			If random.NextBoolean() Then
+				method = type.GetMethod(NameOf(IMaxLength(Of Object).WithMaximumInputLength), New Type() {GetType(Byte)})
+
+				method.Invoke(builder, New Object() {CByte(random.Next(0, 50))})
+			End If
+		End If
+
+		If builder.GetType().ImplementsGenericInterface(GetType(ISizeString(Of ))) Then
+			If random.NextBoolean() Then
+				method = type.GetMethod(NameOf(ISizeString(Of Object).AsWideAs), New Type() {GetType(String)})
+
+				method.Invoke(builder, New Object() {New String("a"c, random.Next(0, 50))})
+			End If
+		End If
+
+		If builder.GetType().ImplementsGenericInterface(GetType(IShowLabel(Of ))) Then
+			If random.NextBoolean() Then
+				If random.NextBoolean() Then
+					method = type.GetMethod(NameOf(IShowLabel(Of Object).ShowLabel), New Type() {})
+
+					method.Invoke(builder, New Object() {})
+				Else
+					method = type.GetMethod(NameOf(IShowLabel(Of Object).HideLabel), New Type() {})
+
+					method.Invoke(builder, New Object() {})
+				End If
+			End If
+		End If
+
+		Return builder.Build()
+	End Function
+
+End Class
