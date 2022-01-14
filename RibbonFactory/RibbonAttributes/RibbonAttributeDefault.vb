853e76aa28b27ede91fc24ecfc3b7ef153ee34c7
Scott K <georgelarry99@gmail.com>
2022-01-14 16:13:16 +0100

    Add DebuggerDisplay attribute and method.

--- RibbonFactory/RibbonAttributes/RibbonAttributeDefault.vb
+++ RibbonFactory/RibbonAttributes/RibbonAttributeDefault.vb
@@ -1,17 +1,27 @@
-﻿Namespace RibbonAttributes
+﻿Imports RibbonFactory.Utilities
 
-    Friend Class RibbonAttributeDefault(Of T)
-        Inherits RibbonAttributeReadOnly(Of T)
+Namespace RibbonAttributes
+	<DebuggerDisplay("{GetDebuggerDisplay(),nq}")>
+	Friend Class RibbonAttributeDefault(Of T)
+		Inherits RibbonAttributeReadOnly(Of T)
 
-        Public Sub New(value As T, name As AttributeName, category As AttributeCategory)
-            MyBase.New(value, name, category)
-        End Sub
+		Public Sub New(value As T, name As AttributeName, category As AttributeCategory)
+			MyBase.New(value, name, category)
+		End Sub
 
-        Public Overrides ReadOnly Property Xml As String
-            Get
-                Return String.Empty
-            End Get
-        End Property
-    End Class
+		Public Overrides ReadOnly Property Xml As String
+			Get
+				Return String.Empty
+			End Get
+		End Property
 
-End NameSpace
+		Private Function GetDebuggerDisplay() As String
+			If TypeOf Value Is Boolean Then
+				Return String.Format(XmlTemplate, Name.CamelCase(), Value.CamelCase())
+			Else
+				Return String.Format(XmlTemplate, Name.CamelCase(), Value)
+			End If
+		End Function
+	End Class
+
+End Namespace
