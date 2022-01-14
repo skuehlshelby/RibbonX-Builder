7f60600cf30075c7d13bccae7397370112bcb5dd
Scott K <georgelarry99@gmail.com>
2022-01-14 16:23:10 +0100

    Inherit from Container base class, implement IDefaultProvider.

--- RibbonFactory/Containers/ButtonGroup.vb
+++ RibbonFactory/Containers/ButtonGroup.vb
@@ -1,56 +1,49 @@
-﻿
+﻿Imports System.Text.RegularExpressions
 Imports RibbonFactory.ControlInterfaces
 Imports RibbonFactory.RibbonAttributes
 
 Namespace Containers
-    
-    Public NotInheritable Class ButtonGroup
-        Inherits RibbonElement
-        Implements IVisible
-        Implements IEnumerable(Of RibbonElement)
 
-        Private ReadOnly _attributes As AttributeSet
-        Private ReadOnly _items As IEnumerable(Of RibbonElement)
-        
-        Friend Sub New(items As IEnumerable(Of RibbonElement), attributes As AttributeSet, Optional tag As Object = Nothing)
-            MyBase.New(tag)
-            _attributes = attributes
-            _items = items
-            AddHandler _attributes.AttributeChanged, AddressOf RefreshNeeded
-        End Sub
-        
-        Public Overrides ReadOnly Property ID As String
-            Get
-                Return _attributes.ReadOnlyLookup(Of String)(AttributeName.Id).GetValue()
-            End Get
-        End Property
+	Public NotInheritable Class ButtonGroup
+		Inherits Container(Of RibbonElement)
+		Implements IVisible
+		Implements IDefaultProvider
 
-        Public Overrides ReadOnly Property XML As String
-            Get
-                Return _
-                    String.Join(Environment.NewLine, $"<buttonGroup { _attributes }>",
-                                String.Join(Environment.NewLine, _items), "</buttonGroup")
-            End Get
-        End Property
+		Private ReadOnly _attributes As AttributeSet
 
-        Public Property Visible As Boolean Implements IVisible.Visible
-            Get
-                Return _attributes.ReadOnlyLookup(Of Boolean)(AttributeName.Visible).GetValue()
-            End Get
-            Set
-                _attributes.ReadWriteLookup(Of Boolean)(AttributeName.GetVisible).SetValue(Value)
-                
-            End Set
-        End Property
-        
-        Public Function GetEnumerator() As IEnumerator(Of RibbonElement) Implements IEnumerable(Of RibbonElement).GetEnumerator
-            Return _items.GetEnumerator()
-        End Function
+		Friend Sub New(items As ICollection(Of RibbonElement), attributes As AttributeSet, Optional tag As Object = Nothing)
+			MyBase.New(items, tag)
+			_attributes = attributes
+			AddHandler _attributes.AttributeChanged, AddressOf RefreshNeeded
+		End Sub
 
-        Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
-            Return _items.GetEnumerator()
-        End Function
+		Public Overrides ReadOnly Property ID As String
+			Get
+				Return _attributes.ReadOnlyLookup(Of String)(AttributeName.Id).GetValue()
+			End Get
+		End Property
 
-    End Class
-    
-End NameSpace
+		Public Overrides ReadOnly Property XML As String
+			Get
+				Dim regex As Regex = New Regex("(?:size|getSize)=""\w+""")
+
+				Return String.Join(Environment.NewLine, $"<buttonGroup { _attributes }>", regex.Replace(String.Join(Environment.NewLine, Items), String.Empty), "</buttonGroup>")
+			End Get
+		End Property
+
+		Public Property Visible As Boolean Implements IVisible.Visible
+			Get
+				Return _attributes.ReadOnlyLookup(Of Boolean)(AttributeName.Visible).GetValue()
+			End Get
+			Set
+				_attributes.ReadWriteLookup(Of Boolean)(AttributeName.GetVisible).SetValue(Value)
+			End Set
+		End Property
+
+		Private Function GetDefaults() As AttributeSet Implements IDefaultProvider.GetDefaults
+			Return _attributes
+		End Function
+
+	End Class
+
+End Namespace
