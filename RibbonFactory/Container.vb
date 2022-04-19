793fcbbef04bf30a3e0c88dec80f0301484239cd
Scott Kuehl-Shelby <georgelarry99@gmail.com>
2022-04-19 14:50:24 +0200

    Simplify container overrides.

--- RibbonFactory/Container.vb
+++ RibbonFactory/Container.vb
@@ -1,15 +1,16 @@
-﻿Imports System.Reflection
+﻿
+Imports System.Reflection
 Imports RibbonFactory.Utilities
 
 Public MustInherit Class Container(Of TRibbonElement As RibbonElement)
     Inherits RibbonElement
     Implements ICollection(Of TRibbonElement)
-    
+
     Protected ReadOnly Items As ICollection(Of TRibbonElement)
 
     Protected Sub New(items As ICollection(Of TRibbonElement), Optional tag As Object = Nothing)
         MyBase.New(tag)
-        Me.Items = If(items, New TRibbonElement() {})
+        Me.Items = If(items, New List(Of TRibbonElement)())
     End Sub
 
     Friend Overridable Sub Flatten(results As ICollection(Of RibbonElement))
@@ -21,14 +22,63 @@ Public MustInherit Class Container(Of TRibbonElement As RibbonElement)
             If item.GetType().IsDerivedFromGenericType(genericContainer) Then
                 Dim method As MethodBase = item.GetType().GetMethod("Flatten", BindingFlags.Instance Or BindingFlags.NonPublic)
 
-                method.Invoke(item, New Object(){results})
+                method.Invoke(item, New Object() {results})
             Else
                 results.Add(item)
             End If
         Next
     End Sub
 
-#Region "ICollection Members"
+#Region "Add"
+
+    Public Sub Add(item As TRibbonElement) Implements ICollection(Of TRibbonElement).Add
+        BeforeAdd(item)
+        Items.Add(item)
+        AfterAdd(item)
+    End Sub
+
+    Protected Overridable Sub BeforeAdd(item As TRibbonElement)
+    End Sub
+
+    Protected Overridable Sub AfterAdd(item As TRibbonElement)
+    End Sub
+
+#End Region
+
+#Region "Clear"
+
+    Public Sub Clear() Implements ICollection(Of TRibbonElement).Clear
+        If Items.Any() Then
+            BeforeClear(Items)
+            Items.Clear()
+            AfterClear()
+        End If
+    End Sub
+
+    Protected Overridable Sub BeforeClear(currentItems As ICollection(Of TRibbonElement))
+    End Sub
+
+    Protected Overridable Sub AfterClear()
+    End Sub
+
+#End Region
+
+#Region "Remove"
+
+    Public Function Remove(item As TRibbonElement) As Boolean Implements ICollection(Of TRibbonElement).Remove
+        BeforeRemove(item)
+        Dim result As Boolean = Items.Remove(item)
+        AfterRemove(item)
+        Return result
+    End Function
+
+    Protected Overridable Sub BeforeRemove(item As TRibbonElement)
+    End Sub
+
+    Protected Overridable Sub AfterRemove(item As TRibbonElement)
+    End Sub
+
+#End Region
 
     Public ReadOnly Property Count As Integer Implements ICollection(Of TRibbonElement).Count
         Get
@@ -42,18 +92,6 @@ Public MustInherit Class Container(Of TRibbonElement As RibbonElement)
         End Get
     End Property
 
-    Public Overridable Sub Add(item As TRibbonElement) Implements ICollection(Of TRibbonElement).Add
-        Items.Add(item)
-        RefreshNeeded()
-    End Sub
-
-    Public Overridable Sub Clear() Implements ICollection(Of TRibbonElement).Clear
-        If Items.Any() Then
-            Items.Clear()
-            RefreshNeeded()
-        End If
-    End Sub
-
     Public Sub CopyTo(array() As TRibbonElement, arrayIndex As Integer) Implements ICollection(Of TRibbonElement).CopyTo
         Items.CopyTo(array, arrayIndex)
     End Sub
@@ -62,39 +100,8 @@ Public MustInherit Class Container(Of TRibbonElement As RibbonElement)
         Return Items.Contains(item)
     End Function
 
-    Public Overridable Overloads Function Remove(item As TRibbonElement) As Boolean Implements ICollection(Of TRibbonElement).Remove
-        If Items.Remove(item) Then
-            RefreshNeeded()
-            Return True
-        Else
-            Return False
-        End If
-    End Function
 
-    Public Overridable Overloads Function Remove(selector As Func(Of TRibbonElement, Boolean)) As Boolean
-        If Items.Any(selector) Then
-            Items.Remove(Items.Single(selector))
-            RefreshNeeded()
-            Return True
-        Else
-            Return False
-        End If
-    End Function
-
-    Public Overridable Overloads Sub Replace(original As TRibbonElement, replacement As TRibbonElement)
-        If Items.Remove(original) Then
-            Items.Add(replacement)
-            RefreshNeeded()
-        End If
-    End Sub
-
-    Public Overridable Overloads Sub Replace(selector As Func(Of TRibbonElement, Boolean), replacement As TRibbonElement)
-        If Items.Any(selector) Then
-            Items.Remove(Items.Single(selector))
-            Items.Add(replacement)
-            RefreshNeeded()
-        End If
-    End Sub
+#Region "IEnumerable Members"
 
     Public Overridable Function GetEnumerator() As IEnumerator(Of TRibbonElement) Implements IEnumerable(Of TRibbonElement).GetEnumerator
         Return Items.GetEnumerator()
@@ -105,5 +112,6 @@ Public MustInherit Class Container(Of TRibbonElement As RibbonElement)
     End Function
 
 #End Region
+
 
 End Class
