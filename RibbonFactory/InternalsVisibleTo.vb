d779ab3e7649bc54b8a0442f7d3cfe96527d8d17
Scott Kuehl-Shelby <georgelarry99@gmail.com>
2022-04-19 14:48:50 +0200

    Remove InternalsVisibleTo assembly attribute. Tests no longer need access to internal members.

--- RibbonFactory/InternalsVisibleTo.vb
+++
@@ -1,7 +0,0 @@
-﻿#If DEBUG Then
-Imports InternalsVisibleTo = System.Runtime.CompilerServices.InternalsVisibleToAttribute
-''<Assembly: InternalsVisibleTo("UnitTests", AllInternalsVisible:=True)>
-#End If
-Friend Module InternalsVisible
-
-End Module
