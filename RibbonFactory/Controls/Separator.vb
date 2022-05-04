e91d8ad5023f778bfce6081aef637b89787b60b8
Scott Kuehl-Shelby <georgelarry99@gmail.com>
2022-05-04 16:59:00 +0200

    Add convenience function to create a simple separator.

--- RibbonFactory/Controls/Separator.vb
+++ RibbonFactory/Controls/Separator.vb
@@ -12,20 +12,22 @@ Namespace Controls
 
         Private ReadOnly _attributes As AttributeSet
 
-        Public Sub New(Optional tag As Object = Nothing)
-            Me.New(Nothing, Nothing, tag)
+        Public Sub New()
+            Me.New(Nothing, Nothing, Nothing)
         End Sub
 
         Public Sub New(configuration As Action(Of ISeparatorBuilder), Optional tag As Object = Nothing)
-            Me.New(Nothing, configuration, tag)
+            Me.New(configuration, Nothing, tag)
         End Sub
 
-        Public Sub New(template As RibbonElement, configuration As Action(Of ISeparatorBuilder), Optional tag As Object = Nothing)
+        Public Sub New(configuration As Action(Of ISeparatorBuilder), template As RibbonElement, Optional tag As Object = Nothing)
             MyBase.New(tag)
 
-            Dim builder As SeparatorBuilder = If(template Is Nothing, New SeparatorBuilder(), New SeparatorBuilder(template))
+            Dim builder As SeparatorBuilder = New SeparatorBuilder(template)
 
-            configuration.Invoke(builder)
+            If configuration IsNot Nothing Then
+                configuration.Invoke(builder)
+            End If
 
             _attributes = builder.Build()
 
@@ -52,6 +54,20 @@ Namespace Controls
                 _attributes.Write(Value, AttributeCategory.Visibility)
             End Set
         End Property
+
+        Public Shared Function CreateVisible(Optional additionalConfiguration As Action(Of ISeparatorBuilder) = Nothing) As Separator
+            Dim invisibleAction As Action(Of ISeparatorBuilder) = Sub(sb) sb.Visible()
+            Dim configuration As Action(Of ISeparatorBuilder) = DirectCast([Delegate].Combine(invisibleAction, additionalConfiguration), Action(Of ISeparatorBuilder))
+
+            Return New Separator(configuration)
+        End Function
+
+        Public Shared Function CreateInvisible(Optional additionalConfiguration As Action(Of ISeparatorBuilder) = Nothing) As Separator
+            Dim invisibleAction As Action(Of ISeparatorBuilder) = Sub(sb) sb.Invisible()
+            Dim configuration As Action(Of ISeparatorBuilder) = DirectCast([Delegate].Combine(invisibleAction, additionalConfiguration), Action(Of ISeparatorBuilder))
+
+            Return New Separator(configuration)
+        End Function
 
         Private Function GetDefaults() As AttributeSet Implements IDefaultProvider.GetDefaults
             Return _attributes.Clone()
