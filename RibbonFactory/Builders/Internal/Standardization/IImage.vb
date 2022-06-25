392c3c1ee42f93d2d625026250d6cd4c00404685
Scott Kuehl-Shelby <georgelarry99@gmail.com>
2022-06-25 22:20:41 +0200

    Added comments for cached image functions.

--- RibbonFactory/Builders/Internal/Standardization/IImage.vb
+++ RibbonFactory/Builders/Internal/Standardization/IImage.vb
@@ -11,8 +11,38 @@ Namespace Builders.Internal.Standardization
 
         Function WithImage(image As ImageMSO, callback As FromControl(Of String)) As T
 
+        ''' <summary>
+        ''' <para>
+        '''  Create a RibbonImage with a custom image whose contents will be queried by the MS Office <br/>
+        '''  host only once. This retrieval will happen during the 'loadImage' callback. Subsequent control<br/>
+        '''  refresh events will not update the image.
+        ''' </para>
+        ''' <br/>
+        ''' <para>
+        '''  Use this method when you want to use a custom image that you do not plan on changing at <br/>
+        ''' runtime.
+        ''' </para>
+        ''' </summary>
+        ''' <param name="imageId">A unique identifier for this image.</param>
+        ''' <param name="image">The image.</param>
+        ''' <returns></returns>
         Function WithImage(imageId As String, image As Bitmap) As T
 
+        ''' <summary>
+        ''' <para>
+        '''  Create a RibbonImage with a custom image whose contents will be queried by the MS Office <br/>
+        '''  host only once. This retrieval will happen during the 'loadImage' callback. Subsequent control<br/>
+        '''  refresh events will not update the image.
+        ''' </para>
+        ''' <br/>
+        ''' <para>
+        '''  Use this method when you want to use a custom image that you do not plan on changing at <br/>
+        ''' runtime.
+        ''' </para>
+        ''' </summary>
+        ''' <param name="imageId">A unique identifier for this image.</param>
+        ''' <param name="image">The image.</param>
+        ''' <returns></returns>
         Function WithImage(imageId As String, image As Icon) As T
 
         Function WithImage(image As Bitmap, callback As FromControl(Of IPictureDisp)) As T
